26515527c60d73a14c7a0fde3ee8453232355496
Shane Quick <shanesfast@gmail.com>
2019-11-25 16:03:33 -0600

    Remove unnecesary code. Updated algorithm to only start from the right side and skip the first digit of card number input.

--- spec/luhn_spec.rb
+++ spec/luhn_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe Luhn, type: :model do
     # some credit card numbers are 15 digits
     # these tests confirm the code is starting on the right side, not the left
     it 'should return true if the number is valid and the number has an odd number of digits' do
-      expect(Luhn.is_valid?(377681478627336)).to be false
+      expect(Luhn.is_valid?(377681478627336)).to be true
     end
 
     it 'should return false if the number is not valid and the number has an odd number of digits' do
